89ba07d079811fbb66d283d1dda40ff932e23072
Pavel Savkin <savkin@azoft.com>
2017-02-26 16:27:29 +0700

    Added empty favicon.ico response.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
+    if (req.url === '/favicon.ico') {
+        r.writeHead(200, { 'Content-Type': 'image/x-icon' });
+        r.end();
+        return;
+    }
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
